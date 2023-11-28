f1120a98d2e4e8c431e43d36b949c831d7682b76
nomi <nomihalpert@gmail.com>
2023-11-28 11:40:51 +0200

    push my project

---
+++ server/server/Controllers/EntrancesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using server.Models;
+using System.Drawing;
+using System.Text;
+using System.Text.Json;
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EntrancesController : Controller
+    {
+        public IConfiguration _configuration { get; set; }
+        public string Path { get; set; }
+        public EntrancesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            Path = _configuration.GetSection("Path").GetSection("JsonPath").Value;
+        }
+        public TicketsAvailability[] TicketsAvailability { get; set; }
+        [HttpGet]
+        public ActionResult<TicketsAvailability[]> GetTicketsAvailability()
+        {
+            try
+            {
+                string json = System.IO.File.ReadAllText(Path);
+                TicketsAvailability = JsonSerializer.Deserialize<TicketsAvailability[]>(json);
+                return TicketsAvailability;
+            }
+            catch (IOException ex)
+            {
+                throw (ex);
+            }
+        }
+        [HttpPut]
+        [Route("{index}")]
+        public ActionResult<TicketsAvailability[]> TicketPurchase([FromBody] int numOfTickets, [FromRoute] int index)
+        {
+            try
+            {
+                //call GetTicketsAvailability function in order to get data and update it.
+                GetTicketsAvailability();
+                TicketsAvailability[index].availablePlaces -= numOfTickets;
+                var json = JsonSerializer.Serialize(TicketsAvailability);
+                System.IO.File.WriteAllText(Path, json);
+                return TicketsAvailability;
+            }
+            catch (IOException ex)
+            {
+                throw (ex);
+            }
+        }
+    }
+}
---
+++ server/server/Models/TicketsAvailability.cs
@@ -0,0 +1,9 @@
+﻿namespace server.Models
+{
+    public class TicketsAvailability
+    {
+        public string startTime { get; set; }
+        public int availablePlaces { get; set; }
+        public bool isClosed { get; set; }
+    }
+}
